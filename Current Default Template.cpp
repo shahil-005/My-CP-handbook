7ced2d42d7848f90b22f744a492bd6b9563f0010
shahil-005 <53808488+shahil-005@users.noreply.github.com>
2020-01-31 17:26:31 +0530

    Create Current Default Template.cpp

---
+++ Current Default Template.cpp
@@ -0,0 +1,17 @@
+//Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{   ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+   
+    }
+    return 0;
+}
