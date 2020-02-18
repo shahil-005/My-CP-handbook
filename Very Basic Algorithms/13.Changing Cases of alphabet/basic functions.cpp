6e379b5cf861f8e92ce0384b9dce641a2ee3ee00
Shahil Singh <53808488+shahil-005@users.noreply.github.com>
2020-02-19 03:50:16 +0530

    Create basic functions.cpp

---
+++ Very Basic Algorithms/13.Changing Cases of alphabet/basic functions.cpp
@@ -0,0 +1,6 @@
+//toupper() : https://www.geeksforgeeks.org/toupper-function-in-c/
+//tolower() : https://www.geeksforgeeks.org/tolower-function-in-c/
+//towupper() : https://www.geeksforgeeks.org/towupper-function-in-c-c/
+//towlower() : https://www.geeksforgeeks.org/towlower-function-in-c-c/
+//isupper() : https://www.geeksforgeeks.org/isupper-islower-application-c/
+//islower() : https://www.geeksforgeeks.org/isupper-islower-application-c/
