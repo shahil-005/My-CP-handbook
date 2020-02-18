1c32d45217d5c4846c25afe5b9e5c1c7fb6db122
Shahil Singh <53808488+shahil-005@users.noreply.github.com>
2020-02-19 03:42:18 +0530

    Update Current Default Template.cpp

--- Current Default Template.cpp
+++ Current Default Template.cpp
@@ -1,17 +1,21 @@
+//shahil_005
 //Some basic pre-written functions might have been copied from www.geeksforgeeks.org or cp-algorithms.com//
 
 #include <bits/stdc++.h>
-#include <iostream>
+
 using namespace std;
 
+#define endl '\n'
+#define int long long
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
+const int N = 1e5 + 5;
+const long long MOD=(long long)(1e9+7);
+const long long INIT=(long long)(1e6+1);
 
-int main()
-{   ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+int32_t main()
+{
+    
 
-   
-}
-    return 0;
+	return 0;
 }
